55481534c4549adfac83fd5b7eda5a979818251d
David Parker <parkers.postbox@gmail.com>
2025-01-08 10:10:47 +0000

    First Commit Site Draft

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from datetime import date
+
+app = Flask(__name__)
+@app.route('/')
+def index() -> str:
+    return render_template('index.html', year=date.today().strftime('%Y'))
+
+
+@app.route('/projects')
+def projects() -> str:
+    #return render_template('projects.html')
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/assets/js/scripts.js
@@ -0,0 +1,54 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// Active Nav Link on Scroll
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 200;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('li a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    })
+};
